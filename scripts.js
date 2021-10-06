9983014a0f9b8563b91c09af4e5f644980284622
Grishma19 <91224283+Grishma19@users.noreply.github.com>
2021-10-06 13:52:37 -0400

    Files

---
+++ scripts.js
@@ -0,0 +1,100 @@
+function setup(){
+    createCanvas(800,1000)
+}
+
+function draw(){
+  background('fffffff')
+
+  fill("white");
+  strokeWeight(4);
+  rect(0, 0, 100, 300);
+
+  fill("green");
+  strokeWeight(4);
+  rect(100, 0, 300, 100);
+
+  fill("white");
+  strokeWeight(4);
+  rect(100,100,200,200);
+  
+  fill("white");
+  strokeWeight(4);
+  rect(400, 0,100,100)
+
+  fill("white");
+  strokeWeight(4);
+  rect(500, 0, 200, 100);
+
+  fill("white");
+  strokeWeight(4);
+  rect(700, 0, 100, 200)
+
+  fill("red");
+  strokeWeight(4);
+  rect(300, 100, 200, 300);
+
+  fill("black");
+  strokeWeight(4);
+  rect(500, 100, 200, 200);
+
+  fill("white");
+  strokeWeight(4);
+  line(500, 200, 700, 200);
+
+  fill("yellow");
+  strokeWeight(4);
+  rect(0,300, 300, 300);
+
+  fill("white");
+  strokeWeight(4);
+  rect(300, 400, 200, 200);
+
+  fill("white");
+  strokeWeight(4);
+  rect(500, 300, 200, 400)
+
+  fill("yellow");
+  strokeWeight(4);
+  rect(700, 200, 100, 400)
+
+  fill("white");
+  strokeWeight(4);
+  rect(700, 600, 100, 100);
+
+  fill("white");
+  strokeWeight(4);
+  rect(0, 600, 500, 200);
+
+  fill("white");
+  strokeWeight(4);
+  line(0, 700, 500, 700);
+
+  fill("blue");
+  strokeWeight(4);
+  rect(500, 700, 200,200)
+
+  fill("white");
+  strokeWeight(4);
+  rect(500, 900, 300, 100)
+
+  fill("white");
+  strokeWeight(4);
+  rect(700, 700, 100, 200)
+
+  fill("white");
+  strokeWeight(4);
+  rect(300, 800, 200, 200)
+
+  fill("black");
+  strokeWeight(4);
+  rect(200, 800, 100, 200)
+
+  fill("white");
+  strokeWeight(4);
+  rect(0, 800, 200, 200)
+
+  fill("white");
+  strokeWeight(4);
+  line(300, 600, 300, 700)
+
+}
